53ccdb8b24d01f2f71209e641e06aaa5abc80238
AitaddiAB <AitaddiAB@gmail.com>
2024-11-23 19:43:28 +0100

    initialize tauri

---
+++ src-tauri/gen/android/app/src/main/java/com/autorun/tauri/MainActivity.kt
@@ -0,0 +1,3 @@
+package com.autorun.tauri
+
+class MainActivity : TauriActivity()
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import os from "os";
+
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+const localIP = () =>
+  Object.values(os.networkInterfaces())
+    .flat()
+    .find((i) => i.family === "IPv4" && !i.internal)?.address || "localhost";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  server: {
+    port: 8089,
+    host: localIP(),
+  },
+});
